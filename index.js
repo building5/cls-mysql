3a559ff8a3e6bfc277587286386a5e6540c2c395
David M. Lee <dlee@digium.com>
2015-02-25 00:56:43 -0600

    Shims for connection and connection pool

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 // Copyright (c) 2015. David M. Lee, II
 'use strict';
 
+var shimmer = require('shimmer');
+
+var Protocol = require('mysql/lib/protocol/Protocol');
+var Pool = require('mysql/lib/Pool');
+
 module.exports = function(ns) {
-  // TODO
+  shimmer.wrap(Protocol.prototype, '_enqueue', function(enqueue) {
+    return function(sequence) {
+      sequence._callback = ns.bind(sequence._callback);
+      return enqueue.call(this, sequence);
+    };
+  });
+
+  shimmer.wrap(Pool.prototype, 'getConnection', function(getConnection) {
+    return function(cb) {
+      return getConnection.call(this, ns.bind(cb));
+    };
+  });
 };
